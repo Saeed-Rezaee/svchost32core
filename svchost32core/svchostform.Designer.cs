4a187d35c7607a7089d56d445da2b0513bd2dd13
Mirza Ghulam Rasyid <mgr@voxteneo.asia>
2020-04-27 22:20:58 +0700

    hide window

--- svchost32core/svchostform.Designer.cs
+++ svchost32core/svchostform.Designer.cs
@@ -35,8 +35,9 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(197, 65);
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "svchostform";
+            this.Opacity = 0D;
             this.ShowIcon = false;
             this.ResumeLayout(false);
 
